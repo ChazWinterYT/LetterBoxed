289a72c5b3dde070524601e80495d3051beae18d
Chaz Winter <chas@hmrmusic.com>
2024-11-12 05:54:46 -0500

    Created lambda stubs

--- backend/lambda/common/game_utils.py
+++ backend/lambda/common/game_utils.py
@@ -1,11 +1,20 @@
-import re
 from uuid import uuid4
-import 
+import hashlib
 
-def generate_game_id(letters, is_random=False, max_retries=3):
+def generate_game_id(sides, is_random=False, max_retries=3):
     """
-    Generate unique game ID based on letters and game type
+    Create a standardized layout and hash it to generate a unique game ID
     """
-    retries = 0
-    while retries < max_retries:
-        game_id = 
+    standardized_layout = standardize_board(sides)
+    game_id = hashlib.sha256(standardized_layout.encode()).hexdigest()
+    return game_id, standardized_layout
+
+
+def standardize_board(sides):
+    """
+    Alphabetize the letters on each side of the board and create a 
+    standardized layout string
+    """
+    standardized_sides = ["".join(sorted(side)) for side in sides]
+    standardized_layout = ["-".join(standardized_sides)]
+    return standardized_layout
