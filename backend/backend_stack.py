857e2e666c2666714a7741eb7bc1e74a6583eab7
Chaz Winter <denonno@amazon.com>
2024-11-13 19:19:55 -0500

    Change DB schema to cache game solutions

--- backend/backend_stack.py
+++ backend/backend_stack.py
@@ -1,6 +1,5 @@
 from aws_cdk import (
     Stack,
-    aws_sqs as sqs,
     aws_dynamodb as dynamodb,
     aws_lambda as _lambda
 )
@@ -10,26 +9,27 @@ class LetterBoxedStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # DynamoDB table definition
+        # DynamoDB table definition for unique games and standardized solutions
         self.game_table = dynamodb.Table(
             self, "LetterBoxedGamesTable",
             table_name="LetterBoxedGames",
             partition_key=dynamodb.Attribute(
-                name="gameid",
+                name="gameId",          # Unique identifier for each specific game layout
                 type=dynamodb.AttributeType.STRING
             ),
             billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST
         )
-        # Global Secondary Index for userid
+
+        # Global Secondary Index (GSI) for standardized game layouts (equivalent solutions)
         self.game_table.add_global_secondary_index(
-            index_name="UserIdIndex",
+            index_name="StandardizedHashIndex",
             partition_key=dynamodb.Attribute(
-                name="userId",
+                name="standardizedHash",  # Unique identifier for the standardized version of the layout
                 type=dynamodb.AttributeType.STRING
             )
         )
 
-        # Lambda for word validation
+        # Lambda function for word validation
         self.validate_word_function = _lambda.Function(
             self, "ValidateWordFunction",
             runtime=_lambda.Runtime.PYTHON_3_10,
@@ -37,6 +37,5 @@ class LetterBoxedStack(Stack):
             code=_lambda.Code.from_asset("lambda")
         )
 
-        # Grant DynamoDB access to the Lambda
+        # Grant read/write permissions to the Lambda function
         self.game_table.grant_read_write_data(self.validate_word_function)
-        
