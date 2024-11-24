e6325a461ac2076b314cf9d92cdf16b9546592df
Chaz Winter <denonno@amazon.com>
2024-11-24 14:28:50 -0500

    Initialize index page of react app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,12 @@
-import React, { useState } from 'react';
-import Header from './components/Header';
-import LeftMenu from './components/LeftMenu';
-import GameBoard from './components/GameBoard';
-import Footer from './components/Footer';
-import './App.css';
+import React from 'react';
 
-const App = () => {
-  const [board, setBoard] = useState<string[]>([]);
-
-  const handleMenuSelect = (option: string) => {
-    console.log(`Selected: ${option}`);
-    // Fetch the board based on the selected option
-    if (option === 'random') setBoard(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']);
-    if (option === 'custom') setBoard([]);
-  };
-
+function App() {
   return (
-    <div className="app">
-      <Header />
-      <div className="main-container">
-        <LeftMenu onOptionSelect={handleMenuSelect} />
-        <main className="game-container">
-          <GameBoard board={board} />
-        </main>
-      </div>
-      <Footer />
+    <div>
+      <h1>Welcome to Stitch-uations!</h1>
+      <p>This is a LetterBoxed game!</p>
     </div>
   );
-};
+}
 
 export default App;
