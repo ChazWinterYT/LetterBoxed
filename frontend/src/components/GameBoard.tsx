d0bce002aa157db90eeecd6911840a5b29550e46
Chaz Winter <denonno@amazon.com>
2024-11-24 20:57:06 -0500

    Can select letters on gme board to form words

--- frontend/src/components/GameBoard.tsx
+++ frontend/src/components/GameBoard.tsx
@@ -1,51 +1,88 @@
-import React from "react";
-import "./css/GameBoard.css";
+import React, { useState } from 'react';
+import './css/GameBoard.css';
 
 interface GameBoardProps {
   layout: string[];
 }
 
 const GameBoard: React.FC<GameBoardProps> = ({ layout }) => {
-  if (!layout || layout.length !== 4 || layout.some((side) => side.length !== 3)) {
-    return <div className="game-board">Invalid game layout</div>;
-  }
+  const [currentWord, setCurrentWord] = useState<string>('');
+
+  const handleLetterClick = (letter: string) => {
+    setCurrentWord((prevWord) => prevWord + letter);
+  };
+
+  const handleDelete = () => {
+    setCurrentWord((prevWord) => prevWord.slice(0, -1));
+  };
+
+  const handleSubmit = () => {
+    alert(`Submitted word: ${currentWord}`);
+    setCurrentWord(''); // Clear the current word
+  };
 
   return (
-    <div className="game-board">
-      {/* Top Side */}
-      <div className="side top">
-        {layout[0].split("").map((letter, index) => (
-          <span key={`top-${index}`} className="letter">
-            {letter}
-          </span>
-        ))}
+    <div className="game-board-container">
+      {/* Word Formation Area */}
+      <div className="word-formation-area">
+        <div className="current-word">{currentWord || '\u00A0'}</div>
       </div>
 
-      {/* Left Side */}
-      <div className="side left">
-        {layout[1].split("").map((letter, index) => (
-          <span key={`left-${index}`} className="letter">
-            {letter}
-          </span>
-        ))}
+      {/* Game Board */}
+      <div className="board">
+        {layout.length === 4 && (
+          <>
+            <div className="top-side">
+              {layout[0].split('').map((letter, index) => (
+                <div
+                  key={`top-${index}`}
+                  className="letter"
+                  onClick={() => handleLetterClick(letter)}
+                >
+                  {letter}
+                </div>
+              ))}
+            </div>
+            <div className="left-side">
+              {layout[1].split('').map((letter, index) => (
+                <div
+                  key={`left-${index}`}
+                  className="letter"
+                  onClick={() => handleLetterClick(letter)}
+                >
+                  {letter}
+                </div>
+              ))}
+            </div>
+            <div className="right-side">
+              {layout[3].split('').map((letter, index) => (
+                <div
+                  key={`right-${index}`}
+                  className="letter"
+                  onClick={() => handleLetterClick(letter)}
+                >
+                  {letter}
+                </div>
+              ))}
+            </div>
+            <div className="bottom-side">
+              {layout[2].split('').map((letter, index) => (
+                <div
+                  key={`bottom-${index}`}
+                  className="letter"
+                  onClick={() => handleLetterClick(letter)}
+                >
+                  {letter}
+                </div>
+              ))}
+            </div>
+          </>
+        )}
       </div>
-
-      {/* Bottom Side */}
-      <div className="side bottom">
-        {layout[2].split("").map((letter, index) => (
-          <span key={`bottom-${index}`} className="letter">
-            {letter}
-          </span>
-        ))}
-      </div>
-
-      {/* Right Side */}
-      <div className="side right">
-        {layout[3].split("").map((letter, index) => (
-          <span key={`right-${index}`} className="letter">
-            {letter}
-          </span>
-        ))}
+      {/* Controls */}
+      <div className="controls">
+        <button onClick={handleDelete}>Delete Letter</button>
+        <button onClick={handleSubmit}>Submit Word</button>
       </div>
     </div>
   );
