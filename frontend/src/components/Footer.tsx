219810698792d9b594a1ab35013e21f06dafe282
Chaz Winter <denonno@amazon.com>
2024-11-25 09:46:31 -0500

    Added link in footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import './css/Footer.css';
 
 const Footer = () => (
   <footer className="footer">
-    <p>© 2024 chazwinter.com</p>
+    <p><a href="http://www.chazwinter.com">© 2024 chazwinter.com</a></p>
   </footer>
 );
 
