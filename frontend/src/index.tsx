d2422643e9ef87821038d636c2f49b6ed5e51bd3
Chaz Winter <denonno@amazon.com>
2024-11-24 19:38:30 -0500

    createRoot fixes deprecation issue in index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'; // Main App component
+import { createRoot } from 'react-dom/client'; // Import createRoot
+import App from './App';
+import './index.css';
 
-// Render the App into the #root div in public/index.html
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!); // Use createRoot for React 18+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
