28b5d78858b7cd35273fedd53f064d800e742296
Chaz Winter <denonno@amazon.com>
2024-11-24 22:10:03 -0500

    Added UI support for several more languages

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,11 +4,15 @@ import './css/Header.css';
 const Header = () => (
   <header className="header">
     <div className="logo">chazwinter.com</div>
-    <h1>Stitch-uations - A LetterBoxed game!</h1>
+    <h2>Stitch-uations - A LetterBoxed Game!</h2>
     <select className="language-selector">
       <option value="en">English</option>
+      <option value="de">Deutsch</option>
       <option value="es">Español</option>
       <option value="fr">Français</option>
+      <option value="it">Italiano</option>
+      <option value="sv">Svenska</option>
+      <option value="pl">Polski</option>
     </select>
   </header>
 );
