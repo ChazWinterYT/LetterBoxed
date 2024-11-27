3c59ed6d2b2d105bdd98eb1fa8c899508078d136
Chaz Winter <denonno@amazon.com>
2024-11-27 13:59:01 -0500

    Cookie warning addd to footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { useLanguage } from '../context/LanguageContext';
 import './css/Footer.css';
 
-const Footer = () => (
-  <footer className="footer">
-    <p><a href="http://chazwinter.com">© 2024 chazwinter.com</a></p>
-  </footer>
-);
+const Footer = () => {
+  const { t } = useLanguage();
+
+  return (
+    <footer className="footer">
+      <p>
+        <a href="http://chazwinter.com">© 2024 chazwinter.com</a>
+      </p>
+      <p className="cookie-warning">
+        {t('footer.cookieWarning')}
+      </p>
+    </footer>
+  );
+};
 
 export default Footer;
