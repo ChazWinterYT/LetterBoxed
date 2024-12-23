2e77a1f7a0ce93060f65667e692c504319a379c1
Chaz Winter <denonno@amazon.com>
2024-12-22 23:57:15 -0500

    BrowseGames component initialized

--- frontend/src/components/BrowseGames/BrowseGames.tsx
+++ frontend/src/components/BrowseGames/BrowseGames.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { useLanguage } from "../../context/LanguageContext";
 import Header from "../Header";
 import Footer from "../Footer";
 
 const BrowseGames: React.FC = () => {
+  const { t } = useLanguage();
+
   return (
     <div>
       <Header />
+      <button 
+        className="menu-button"
+        onClick={() => window.location.href = "/LetterBoxed/frontend"}
+      >
+        {t("ui.menu.returnHome")}
+      </button>
       <div>
         Hi. This component isn't ready yet.
       </div>
